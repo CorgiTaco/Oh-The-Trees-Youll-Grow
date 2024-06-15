00e70a0437d44ccadff56af4596bc517e4d55a45
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-06-14 21:35:39 -0400

    Initial Arch Loom Port

---
+++ fabric/build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+architectury {
+    platformSetupLoomIde()
+    fabric()
+}
+
+val minecraftVersion = project.properties["minecraft_version"] as String
+
+configurations {
+    create("common")
+    create("shadowCommon")
+    compileClasspath.get().extendsFrom(configurations["common"])
+    runtimeClasspath.get().extendsFrom(configurations["common"])
+    getByName("developmentFabric").extendsFrom(configurations["common"])
+}
+
+loom {
+    accessWidenerPath.set(project(":common").loom.accessWidenerPath)
+    mixin.useLegacyMixinAp.set(false)
+}
+
+fabricApi.configureDataGeneration()
+
+dependencies {
+    modImplementation("net.fabricmc:fabric-loader:${project.properties["fabric_loader_version"]}")
+    modApi("net.fabricmc.fabric-api:fabric-api:${project.properties["fabric_api_version"]}+$minecraftVersion")
+
+    "common"(project(":common", "namedElements")) { isTransitive = false }
+    "shadowCommon"(project(":common", "transformProductionFabric")) { isTransitive = false }
+}
+
+tasks {
+    base.archivesName.set(base.archivesName.get() + "-Fabric")
+    processResources {
+        inputs.property("version", project.version)
+
+        filesMatching("fabric.mod.json") {
+            expand(mapOf("version" to project.version))
+        }
+    }
+
+    shadowJar {
+        configurations = listOf(project.configurations.getByName("shadowCommon"))
+        archiveClassifier.set("dev-shadow")
+    }
+
+    remapJar {
+        injectAccessWidener.set(true)
+        inputFile.set(shadowJar.get().archiveFile)
+        dependsOn(shadowJar)
+    }
+
+    jar.get().archiveClassifier.set("dev")
+
+    sourcesJar {
+        val commonSources = project(":common").tasks.sourcesJar
+        dependsOn(commonSources)
+        from(commonSources.get().archiveFile.map { zipTree(it) })
+    }
+}
+
+components {
+    java.run {
+        if (this is AdhocComponentWithVariants)
+            withVariantsFromConfiguration(project.configurations.shadowRuntimeElements.get()) { skip() }
+    }
+}
+
+publishing {
+    publications.create<MavenPublication>("mavenFabric") {
+        artifactId = "${project.properties["archives_base_name"]}" + "-Fabric"
+        from(components["java"])
+    }
+
+    repositories {
+        mavenLocal()
+        maven {
+            val releasesRepoUrl = "https://maven.jt-dev.tech/releases"
+            val snapshotsRepoUrl = "https://maven.jt-dev.tech/snapshots"
+            url = uri(if (project.version.toString().endsWith("SNAPSHOT") || project.version.toString().startsWith("0")) snapshotsRepoUrl else releasesRepoUrl)
+            name = "JTDev-Maven-Repository"
+            credentials {
+                username = project.properties["repoLogin"]?.toString()
+                password = project.properties["repoPassword"]?.toString()
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement.repositories {
+    maven("https://maven.fabricmc.net/")
+    maven("https://maven.architectury.dev/")
+    maven("https://maven.minecraftforge.net/")
+    maven("https://maven.neoforged.net/releases/")
+    gradlePluginPortal()
+}
+
+plugins {
+    id("com.gradle.develocity") version("3.17.4")
+}
+
+develocity.buildScan {
+    termsOfUseUrl = "https://gradle.com/terms-of-service"
+    termsOfUseAgree = "yes"
+}
+
+include("common", "fabric", "forge")
+
+rootProject.name = "Oh The Trees You'll Grow"
